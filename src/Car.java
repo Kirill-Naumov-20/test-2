fc94f1583467444d5b85266c0fa244c725b112ab
KIRILL <sem242694@gmail.com>
2024-06-09 16:15:57 +0200

    firs com

---
+++ src/Car.java
@@ -0,0 +1,12 @@
+public class Car {
+    public void build(){
+        System.out.println("to do it");
+    }
+    public void buy(){
+        System.out.println("yes");
+    }
+    public void carSpeed() {
+        int Speed = 0;
+        System.out.println(Speed);
+    }
+}
